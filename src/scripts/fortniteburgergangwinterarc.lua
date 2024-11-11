af91e671429cedad39b49fd3f0917054ccaa1ce1
Slided <88856468+Slided@users.noreply.github.com>
2024-11-11 16:53:57 -0500

    hi

---
+++ src/scripts/fortniteburgergangwinterarc.lua
@@ -0,0 +1,500 @@
+game.StarterGui:SetCore("SendNotification", {
+    Title = "fortniteburgergangwinterarc.gg";
+    Text = "Thanks For Executing!";
+    Duration = 3;
+})
+
+print("github.com/slided")
+
+local G2L = {};
+
+G2L["1"] = Instance.new("ScreenGui", game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"));
+G2L["1"]["Name"] = [[flamingobannedonroblox]];
+G2L["1"]["ZIndexBehavior"] = Enum.ZIndexBehavior.Sibling;
+
+
+G2L["2"] = Instance.new("Frame", G2L["1"]);
+G2L["2"]["BorderSizePixel"] = 0;
+G2L["2"]["BackgroundColor3"] = Color3.fromRGB(51, 51, 51);
+G2L["2"]["Size"] = UDim2.new(0, 300, 0, 35);
+G2L["2"]["Position"] = UDim2.new(0.1863, 0, 0.19287, 0);
+G2L["2"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["2"]["Name"] = [[incognitogg]];
+
+
+G2L["3"] = Instance.new("Frame", G2L["2"]);
+G2L["3"]["BorderSizePixel"] = 0;
+G2L["3"]["BackgroundColor3"] = Color3.fromRGB(51, 51, 51);
+G2L["3"]["Size"] = UDim2.new(0, 300, 0, 465);
+G2L["3"]["Position"] = UDim2.new(-0.00036, 0, 0.99287, 0);
+G2L["3"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["3"]["Name"] = [[main]];
+
+
+G2L["4"] = Instance.new("ImageLabel", G2L["3"]);
+G2L["4"]["BorderSizePixel"] = 0;
+G2L["4"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["4"]["ImageTransparency"] = 0.83;
+G2L["4"]["Image"] = [[http://www.roblox.com/asset/?id=108219657565083]];
+G2L["4"]["Size"] = UDim2.new(0, 300, 0, 465);
+G2L["4"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["4"]["BackgroundTransparency"] = 1;
+G2L["4"]["Name"] = [[mainbackground]];
+
+
+G2L["5"] = Instance.new("Frame", G2L["4"]);
+G2L["5"]["BorderSizePixel"] = 0;
+G2L["5"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["5"]["Size"] = UDim2.new(0, 286, 0, 448);
+G2L["5"]["Position"] = UDim2.new(0.0237, 0, 0.01559, 0);
+G2L["5"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["5"]["Name"] = [[innerborder]];
+G2L["5"]["BackgroundTransparency"] = 1;
+
+
+G2L["6"] = Instance.new("Frame", G2L["5"]);
+G2L["6"]["BorderSizePixel"] = 0;
+G2L["6"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["6"]["Size"] = UDim2.new(0, 286, 0, 448);
+G2L["6"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["6"]["Name"] = [[mainworkspace]];
+G2L["6"]["BackgroundTransparency"] = 1;
+
+
+G2L["7"] = Instance.new("Folder", G2L["6"]);
+G2L["7"]["Name"] = [[tmobilepunchgod]];
+
+
+G2L["8"] = Instance.new("TextButton", G2L["7"]);
+G2L["8"]["BorderSizePixel"] = 2;
+G2L["8"]["TextSize"] = 14;
+G2L["8"]["TextColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["8"]["BackgroundColor3"] = Color3.fromRGB(155, 11, 11);
+G2L["8"]["FontFace"] = Font.new([[rbxasset://fonts/families/SourceSansPro.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+G2L["8"]["Size"] = UDim2.new(0, 22, 0, 22);
+G2L["8"]["Name"] = [[AmongUsRedImpostersFatCockXD696969]];
+G2L["8"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["8"]["Text"] = [[]];
+G2L["8"]["Position"] = UDim2.new(0.1, 0, 0.05376, 0);
+
+
+G2L["9"] = Instance.new("UICorner", G2L["8"]);
+
+
+
+G2L["a"] = Instance.new("UIStroke", G2L["8"]);
+G2L["a"]["ApplyStrokeMode"] = Enum.ApplyStrokeMode.Border;
+G2L["a"]["Thickness"] = 1.6;
+
+
+G2L["b"] = Instance.new("TextBox", G2L["7"]);
+G2L["b"]["CursorPosition"] = -1;
+G2L["b"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["b"]["BorderSizePixel"] = 0;
+G2L["b"]["TextWrapped"] = true;
+G2L["b"]["TextSize"] = 16;
+G2L["b"]["Name"] = [[FREEATTACKONDOGEFOR0ROBUX]];
+G2L["b"]["TextScaled"] = true;
+G2L["b"]["BackgroundColor3"] = Color3.fromRGB(60, 60, 60);
+G2L["b"]["FontFace"] = Font.new([[rbxasset://fonts/families/HighwayGothic.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+G2L["b"]["PlaceholderText"] = [[Walkspeed Multiplier]];
+G2L["b"]["Size"] = UDim2.new(0, 164, 0, 27);
+G2L["b"]["Position"] = UDim2.new(0.25175, 0, 0.04707, 0);
+G2L["b"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["b"]["Text"] = [[]];
+
+
+G2L["c"] = Instance.new("UICorner", G2L["b"]);
+
+
+
+G2L["d"] = Instance.new("UIStroke", G2L["b"]);
+G2L["d"]["ApplyStrokeMode"] = Enum.ApplyStrokeMode.Border;
+G2L["d"]["Thickness"] = 1.6;
+
+
+G2L["e"] = Instance.new("LocalScript", G2L["7"]);
+G2L["e"]["Name"] = [[softplushhellokittyplushieaccessory]];
+
+
+G2L["f"] = Instance.new("Folder", G2L["6"]);
+G2L["f"]["Name"] = [[erpwithme]];
+
+
+G2L["10"] = Instance.new("TextButton", G2L["f"]);
+G2L["10"]["BorderSizePixel"] = 2;
+G2L["10"]["TextSize"] = 14;
+G2L["10"]["TextColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["10"]["BackgroundColor3"] = Color3.fromRGB(155, 11, 11);
+G2L["10"]["FontFace"] = Font.new([[rbxasset://fonts/families/SourceSansPro.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+G2L["10"]["Size"] = UDim2.new(0, 22, 0, 22);
+G2L["10"]["Name"] = [[snoopdog]];
+G2L["10"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["10"]["Text"] = [[]];
+G2L["10"]["Position"] = UDim2.new(0.1, 0, 0.15198, 0);
+
+
+G2L["11"] = Instance.new("UICorner", G2L["10"]);
+
+
+
+G2L["12"] = Instance.new("UIStroke", G2L["10"]);
+G2L["12"]["ApplyStrokeMode"] = Enum.ApplyStrokeMode.Border;
+G2L["12"]["Thickness"] = 1.6;
+
+
+G2L["13"] = Instance.new("LocalScript", G2L["f"]);
+G2L["13"]["Name"] = [[kodiene]];
+
+
+G2L["14"] = Instance.new("TextLabel", G2L["f"]);
+G2L["14"]["TextWrapped"] = true;
+G2L["14"]["BorderSizePixel"] = 0;
+G2L["14"]["BackgroundColor3"] = Color3.fromRGB(60, 60, 60);
+G2L["14"]["TextSize"] = 22;
+G2L["14"]["FontFace"] = Font.new([[rbxasset://fonts/families/HighwayGothic.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+G2L["14"]["TextColor3"] = Color3.fromRGB(159, 159, 159);
+G2L["14"]["Size"] = UDim2.new(0, 164, 0, 27);
+G2L["14"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["14"]["Text"] = [[ESP: v1]];
+G2L["14"]["Name"] = [[logo]];
+G2L["14"]["Position"] = UDim2.new(0.25175, 0, 0.15198, 0);
+
+
+G2L["15"] = Instance.new("UIStroke", G2L["14"]);
+G2L["15"]["ApplyStrokeMode"] = Enum.ApplyStrokeMode.Border;
+G2L["15"]["Thickness"] = 1.6;
+
+
+G2L["16"] = Instance.new("UICorner", G2L["14"]);
+
+
+
+G2L["17"] = Instance.new("TextBox", G2L["f"]);
+G2L["17"]["CursorPosition"] = -1;
+G2L["17"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["17"]["BorderSizePixel"] = 0;
+G2L["17"]["TextWrapped"] = true;
+G2L["17"]["TextSize"] = 12;
+G2L["17"]["Name"] = [[letmepegyouwithaskateboard]];
+G2L["17"]["TextScaled"] = true;
+G2L["17"]["BackgroundColor3"] = Color3.fromRGB(60, 60, 60);
+G2L["17"]["FontFace"] = Font.new([[rbxasset://fonts/families/HighwayGothic.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+G2L["17"]["PlaceholderText"] = [[ESP Icon (Decal ID)]];
+G2L["17"]["Size"] = UDim2.new(0, 145, 0, 19);
+G2L["17"]["Position"] = UDim2.new(0.28322, 0, 0.2301, 0);
+G2L["17"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["17"]["Text"] = [[]];
+
+
+G2L["18"] = Instance.new("UICorner", G2L["17"]);
+
+
+
+G2L["19"] = Instance.new("UIStroke", G2L["17"]);
+G2L["19"]["ApplyStrokeMode"] = Enum.ApplyStrokeMode.Border;
+G2L["19"]["Thickness"] = 1.6;
+
+
+G2L["1a"] = Instance.new("UIStroke", G2L["5"]);
+G2L["1a"]["ApplyStrokeMode"] = Enum.ApplyStrokeMode.Border;
+G2L["1a"]["Thickness"] = 1.6;
+G2L["1a"]["Color"] = Color3.fromRGB(28, 89, 23);
+
+
+G2L["1b"] = Instance.new("UICorner", G2L["5"]);
+
+
+
+G2L["1c"] = Instance.new("ImageLabel", G2L["4"]);
+G2L["1c"]["BorderSizePixel"] = 0;
+G2L["1c"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["1c"]["Image"] = [[rbxassetid://12407435312]];
+G2L["1c"]["Size"] = UDim2.new(0, 300, 0, 202);
+G2L["1c"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["1c"]["BackgroundTransparency"] = 1;
+G2L["1c"]["Name"] = [[bg]];
+G2L["1c"]["Position"] = UDim2.new(0, 0, 0.56559, 0);
+
+
+G2L["1d"] = Instance.new("ImageLabel", G2L["3"]);
+G2L["1d"]["BorderSizePixel"] = 0;
+G2L["1d"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["1d"]["Image"] = [[rbxassetid://10729455634]];
+G2L["1d"]["Size"] = UDim2.new(0, 40, 0, 42);
+G2L["1d"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["1d"]["BackgroundTransparency"] = 0.93;
+G2L["1d"]["Name"] = [[lultrasigma]];
+G2L["1d"]["Position"] = UDim2.new(0.81333, 0, 0.87527, 0);
+
+
+G2L["1e"] = Instance.new("UICorner", G2L["1d"]);
+
+
+
+G2L["1f"] = Instance.new("UIStroke", G2L["1d"]);
+G2L["1f"]["ApplyStrokeMode"] = Enum.ApplyStrokeMode.Border;
+G2L["1f"]["Thickness"] = 1.6;
+
+
+G2L["20"] = Instance.new("LocalScript", G2L["3"]);
+
+
+
+G2L["21"] = Instance.new("Folder", G2L["2"]);
+G2L["21"]["Name"] = [[titlebar]];
+
+
+G2L["22"] = Instance.new("TextLabel", G2L["21"]);
+G2L["22"]["TextWrapped"] = true;
+G2L["22"]["BorderSizePixel"] = 0;
+G2L["22"]["TextStrokeColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["22"]["TextScaled"] = true;
+G2L["22"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["22"]["TextSize"] = 14;
+G2L["22"]["FontFace"] = Font.new([[rbxasset://fonts/families/JosefinSans.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+G2L["22"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["22"]["BackgroundTransparency"] = 1;
+G2L["22"]["Size"] = UDim2.new(0, 191, 0, 35);
+G2L["22"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["22"]["Text"] = [[fortniteburgergangwinterarc.gg]];
+G2L["22"]["Name"] = [[fortniteburgergangforlife]];
+G2L["22"]["Position"] = UDim2.new(0.02333, 0, 0, 0);
+
+
+G2L["23"] = Instance.new("ImageLabel", G2L["21"]);
+G2L["23"]["AnchorPoint"] = Vector2.new(0.5, 0.5);
+G2L["23"]["Image"] = [[rbxassetid://17325853332]];
+G2L["23"]["Size"] = UDim2.new(0, 28, 0, 28);
+G2L["23"]["BackgroundTransparency"] = 1;
+G2L["23"]["Position"] = UDim2.new(0.87333, -50, 1.92857, -50);
+
+
+G2L["24"] = Instance.new("LocalScript", G2L["2"]);
+G2L["24"]["Name"] = [[HideAndOpen]];
+
+
+G2L["25"] = Instance.new("LocalScript", G2L["2"]);
+G2L["25"]["Name"] = [[DragScript]];
+
+
+local function C_e()
+local script = G2L["e"];
+	local multiplierBox = script.Parent:WaitForChild("FREEATTACKONDOGEFOR0ROBUX")
+	local toggleButton = script.Parent:WaitForChild("AmongUsRedImpostersFatCockXD696969")
+	
+	local player = game.Players.LocalPlayer
+	local humanoid = player.Character:WaitForChild("Humanoid")
+	
+	local isOn = false
+	local originalWalkSpeed = humanoid.WalkSpeed
+	local kevingatesfreakymultiplier = 1  
+	
+	local onColor = Color3.fromHex("#1edb1a")
+	local offColor = Color3.fromHex("#db1a1a")
+	
+	multiplierBox:GetPropertyChangedSignal("Text"):Connect(function()
+		local inputNumber = tonumber(multiplierBox.Text)
+		if inputNumber then
+			kevingatesfreakymultiplier = inputNumber
+			print("Multiplier updated to:", kevingatesfreakymultiplier)
+		else
+			warn("Invalid input: Please enter a number")
+		end
+	end)
+	
+	local function toggleButtonState()
+		isOn = not isOn
+		if isOn then
+	
+			toggleButton.BackgroundColor3 = onColor
+			humanoid.WalkSpeed = originalWalkSpeed * kevingatesfreakymultiplier
+		else
+	
+			toggleButton.BackgroundColor3 = offColor
+			humanoid.WalkSpeed = originalWalkSpeed
+		end
+	end
+	
+	toggleButton.MouseButton1Click:Connect(toggleButtonState)
+	
+	game:GetService("RunService").Heartbeat:Connect(function()
+		if isOn then
+			humanoid.WalkSpeed = originalWalkSpeed * kevingatesfreakymultiplier
+		end
+	end)
+	
+end;
+task.spawn(C_e);
+local function C_13()
+local script = G2L["13"];
+	local players = game:GetService("Players")
+	local localPlayer = players.LocalPlayer
+	local button = script.Parent.snoopdog
+	local assetTextBox = script.Parent.letmepegyouwithaskateboard
+	local espEnabled = false
+	local colorOn = Color3.fromHex("#349a0f")
+	local colorOff = Color3.fromHex("#9a0a0a")
+	local defaultAssetID = "rbxassetid://92401567"
+	
+	button.BackgroundColor3 = colorOff
+	assetTextBox.Text = defaultAssetID
+	
+	local function getAssetID()
+		return "rbxassetid://" .. (tonumber(assetTextBox.Text) or 92401567)
+	end
+	
+	local function toggleESP()
+		espEnabled = not espEnabled
+		button.BackgroundColor3 = espEnabled and colorOn or colorOff
+	
+		for _, player in pairs(players:GetPlayers()) do
+			if player ~= localPlayer and player.Character and player.Character:FindFirstChild("Head") then
+				local billboard = player.Character.Head:FindFirstChild("DistanceBillboard")
+				if billboard then
+					billboard.Enabled = espEnabled
+				end
+			end
+		end
+	end
+	
+	local function setupESP(player)
+		if player == localPlayer then return end
+	
+		local function onCharacterAdded(character)
+			local head = character:WaitForChild("Head")
+	
+			local billboard = Instance.new("BillboardGui")
+			billboard.Name = "DistanceBillboard"
+			billboard.Adornee = head
+			billboard.Size = UDim2.new(0, 20, 0, 20)  
+			billboard.StudsOffset = Vector3.new(0, 3, 0)
+			billboard.AlwaysOnTop = true
+			billboard.Enabled = espEnabled
+	
+			local imageLabel = Instance.new("ImageLabel", billboard)
+			imageLabel.Size = UDim2.new(1, 0, 1, 0)
+			imageLabel.BackgroundTransparency = 1
+			imageLabel.Image = getAssetID()
+	
+			billboard.Parent = head
+		end
+	
+		player.CharacterAdded:Connect(onCharacterAdded)
+		if player.Character then
+			onCharacterAdded(player.Character)
+		end
+	end
+	
+	assetTextBox.FocusLost:Connect(function(enterPressed)
+		if enterPressed then
+			local newAssetID = getAssetID()
+			for _, player in pairs(players:GetPlayers()) do
+				if player ~= localPlayer and player.Character and player.Character:FindFirstChild("Head") then
+					local billboard = player.Character.Head:FindFirstChild("DistanceBillboard")
+					if billboard and billboard:FindFirstChildOfClass("ImageLabel") then
+						billboard.ImageLabel.Image = newAssetID
+					end
+				end
+			end
+		end
+	end)
+	
+	button.MouseButton1Click:Connect(toggleESP)
+	
+	for _, player in pairs(players:GetPlayers()) do
+		setupESP(player)
+	end
+	
+	players.PlayerAdded:Connect(setupESP)
+	
+end;
+task.spawn(C_13);
+local function C_20()
+local script = G2L["20"];
+	local button = script.Parent.lultrasigma
+	local notificationActive = false  
+	
+	local function sendNotification()
+		if not notificationActive then
+			notificationActive = true  
+	
+			game.StarterGui:SetCore("SendNotification", {
+				Title = "Notice";
+				Text = "Fortniteburgergangwinterarc.gg is made with love and is built from scratch, No stolen code here...";
+				Duration = 5;
+			})
+	
+			task.delay(5, function()
+				notificationActive = false 
+			end)
+		end
+	end
+	
+	button.MouseEnter:Connect(sendNotification)
+	
+end;
+task.spawn(C_20);
+
+local function C_24()
+local script = G2L["24"];
+	local GuiKey = Enum.KeyCode.RightShift  
+	local frame = script.Parent
+	
+	frame.Visible = true
+	
+	local function toggleFrameVisibility()
+		frame.Visible = not frame.Visible
+	end
+	
+	game:GetService("UserInputService").InputBegan:Connect(function(input, gameProcessed)
+		if input.KeyCode == GuiKey and not gameProcessed then
+			toggleFrameVisibility()
+		end
+	end)
+	
+end;
+task.spawn(C_24);
+
+local function C_25()
+local script = G2L["25"];
+	
+	local UIS = game:GetService('UserInputService')
+	local frame = script.Parent
+	local dragToggle = nil
+	local dragSpeed = 0.1
+	local dragStart = nil
+	local startPos = nil
+	
+	local function updateInput(input)
+		local delta = input.Position - dragStart
+		local position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X,
+			startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+		game:GetService('TweenService'):Create(frame, TweenInfo.new(dragSpeed), {Position = position}):Play()
+	end
+	
+	frame.InputBegan:Connect(function(input)
+		if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) then 
+			dragToggle = true
+			dragStart = input.Position
+			startPos = frame.Position
+			input.Changed:Connect(function()
+				if input.UserInputState == Enum.UserInputState.End then
+					dragToggle = false
+				end
+			end)
+		end
+	end)
+	
+	UIS.InputChanged:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+			if dragToggle then
+				updateInput(input)
+			end
+		end
+	end)
+	
+end;
+task.spawn(C_25);
+
+return G2L["1"], require;
